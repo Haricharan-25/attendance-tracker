b9c02e8148396d9e7b3ed2792d609ec5be4d6c51
Haricharan-25 <charanhari072@gmail.com>
2026-09-14 15:41:10 +0530

    Add health check endpoint at /

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
+def health_check(request):
+    return HttpResponse("OK", status=200)
 
 urlpatterns = [
+    path("", health_check, name="health_check"),
     path("admin/", admin.site.urls),
 
     path(
@@ -25,4 +29,4 @@ urlpatterns = [
         TokenRefreshView.as_view(),
         name="token_refresh",
     ),
-]
+]
