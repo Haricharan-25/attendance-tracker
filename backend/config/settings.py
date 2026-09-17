d1caaa1d5a69eb90a12b70030fea7ed7e75ea22f
Haricharan-25 <charanhari072@gmail.com>
2026-09-17 12:43:11 +0530

    updated settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/6.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
+
+# pyrefly: ignore [missing-import]
 import dj_database_url
 from pathlib import Path
 
